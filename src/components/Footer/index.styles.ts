787096b61d9d41dd0414c50033b8596bb5f55225
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-09-21 10:30:03 -0300

    getting footer styled

--- src/components/Footer/index.styles.ts
+++ src/components/Footer/index.styles.ts
@@ -4,8 +4,31 @@ import { Container } from "../../styles/globals";
 export const FooterContainer = styled.footer`
   margin-top: 4rem;
   background-color: #000;
-  height: 15rem;
+  /* height: 15rem; */
   color: ${(props) => props.theme.white};
 `;
 
-export const FooterContent = styled(Container)``;
+export const FooterContent = styled(Container)`
+  span {
+    text-align: center;
+    display: block;
+    padding: 1.5rem;
+    border-top: 1px #747474 solid;
+  }
+`;
+
+export const FooterLinks = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1.5rem;
+  gap: 1rem;
+
+  a {
+    transition: transform 0.3s;
+
+    &:hover {
+      transform: scale(1.3);
+    }
+  }
+`;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,22 @@
-import { FooterContainer } from "./index.styles";
+import { GithubLogo, LinkedinLogo } from "phosphor-react";
+import { FooterContainer, FooterContent, FooterLinks } from "./index.styles";
 
 export const Footer = () => {
-  return <FooterContainer>Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <FooterLinks>
+          <a href="https://www.linkedin.com/in/dev-pedrosoares/">
+            <LinkedinLogo size={32} color="#fff" />
+          </a>
+          <a href="https://github.com/pedro-soares-nogueira">
+            <GithubLogo size={32} color="#fff" />
+          </a>
+        </FooterLinks>
+        <span>
+          @2023 - All Right Reserved. Designed and Developed by Pedro Soares
+        </span>
+      </FooterContent>
+    </FooterContainer>
+  );
 };
